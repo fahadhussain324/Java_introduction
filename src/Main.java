425cebc64a1a951b87ba1ff083d4a1c158b851d1
Fahad Hussain <fahadhussain324@outlook.com>
2026-04-09 20:49:47 +0100

    added math

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,28 @@
+import java.util.Scanner;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello and welcome!");
-        System.out.println("Hello!");
-        int max_player_count = 10;
-        String playerName = "xDanger_";
-        System.out.println(max_player_count);
-        System.out.println(playerName);
+//        System.out.println("Hello and welcome!");
+//        int max_player_count = 10;
+//        String playerName = "xDanger_";
+//        int life = 27;
+//        System.out.println(life);
+//        System.out.println("Your age is " + life + " and your username is " + playerName);
+//
+//        //input - scanner will import the java util called Scanner, create a variable called scanner
+//        Scanner scanner = new Scanner(System.in);
+//        int number = scanner.nextInt();
+//        System.out.println("You just typed " + number);
+//        String input = scanner.next();
+//        System.out.println("You just typed: " + input);
 
-        int life = 27;
-        System.out.println(life);
+        //rounding
+        System.out.println("Rounding: " + Math.round(6.4));
+        System.out.println("Round down: " + Math.floor(6.4)); //can be float too, even tho it is expecting double
+        System.out.println("Round up: " + Math.ceil(6.4));
+        System.out.println("Exponent " + Math.pow(3, 2));
 
     }
 }
