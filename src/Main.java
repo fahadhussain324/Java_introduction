0802bd1b28acb18b659cdbd9fd0462d98d7c5b46
Fahad Hussain <fahadhussain324@outlook.com>
2026-04-09 19:38:36 +0100

    2nd commit

--- src/Main.java
+++ src/Main.java
@@ -2,12 +2,15 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        System.out.printf("Hello and welcome!");
-        System.out.printf("Hello!");
+        System.out.println("Hello and welcome!");
+        System.out.println("Hello!");
+        int max_player_count = 10;
+        String playerName = "xDanger_";
+        System.out.println(max_player_count);
+        System.out.println(playerName);
 
-        
-
-
+        int life = 27;
+        System.out.println(life);
 
     }
 }
